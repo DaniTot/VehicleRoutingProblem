80ea1cc2c148d225e57c58376f4557028e9e0bff
DaniTot <tothdani1995@gmail.com>
2020-12-11 15:10:47 +0100

    Shit's broken, yo

--- main.py
+++ main.py
@@ -3,24 +3,43 @@ from vrp import VRP
 vrp = VRP()
 
 random = True
-
-if random:
-    vrp.setup_random_data(number_of_customers=15,
-                          number_of_vehicles=2,
-                          vehicle_capacity=10,
-                          x_range=10, y_range=10,
-                          demand_lower=1, demand_higher=10,
-                          seed=420)
-else:
-    vrp.setup_preset_data(file_name="validation_data_A/A-n32-k5.vrp",
-                          number_of_vehicles=5)
+formulation = ["capacitated", "timed"][1]
 
 
-vrp.gap_goal = 0.1
-vrp.subtour_type = 'MTZ'
+# vrp.gap_goal = 0.01
 
-vrp.setup()
 
+if formulation is "capacitated":
+    if random:
+        vrp.subtour_type = ['MTZ', 'DFJ'][1]
+        vrp.setup_random_data(number_of_customers=5,
+                              number_of_vehicles=3,
+                              vehicle_capacity=10,
+                              x_range=10, y_range=10,
+                              demand_lower=1, demand_higher=5,
+                              seed=420)
+    else:
+        vrp.setup_preset_data(file_name="validation_data_A/A-n32-k5.vrp",
+                              number_of_vehicles=5)
+    vrp.CVRP_setup()
+elif formulation is "timed":
+    vrp.subtour_type = 'TW'
+    vrp.time_window = 10
+    vrp.opening_time = 0
+    vrp.closing_time = 30
+    vrp.processing_time = 1
+    if random:
+        vrp.setup_random_data(number_of_customers=5,
+                              number_of_vehicles=3,
+                              vehicle_capacity=10,
+                              x_range=10, y_range=10,
+                              demand_lower=1, demand_higher=5,
+                              seed=420)
+    else:
+        vrp.setup_preset_data(file_name="validation_data_A/A-n32-k5.vrp",
+                              number_of_vehicles=5)
+    vrp.CVRPTW_setup()
+
+print("All set")
 vrp.optimize()
-
 vrp.visualize()
--- vrp.py
+++ vrp.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import itertools
+import sys
 from collections import Counter
 from gurobipy import Model, GRB, quicksum
 
@@ -17,6 +18,7 @@ class VRP:
         lorem ipsun
 
     """
+
     def __init__(self):
 
         self.Q = None
@@ -24,7 +26,13 @@ class VRP:
         self.x_range = None
         self.y_range = None
         self.number_of_customers = None
+        # CVRP
         self.demand_range = None
+        # CVRPTW
+        self.time_window = None
+        self.opening_time = None
+        self.closing_time = None
+        self.processing_time = None
 
         self.K = None
         self.nodes = None
@@ -33,13 +41,18 @@ class VRP:
         self.A = None  # Arcs between depot and customer nodes
         self.c = None  # Cost associated with each arc
         self.q = None  # The weight that has to be delivered to each customer
+        self.e = None  # Time window lower boundary
+        self.l = None  # Time window upper boundary
+        self.p = None  # Processing time
 
         self.model = None
         self.x = None
         self.u = None
+        self.t = None
 
         self.subtour_type = 'DFJ'  # DFJ or MTZ
-        self.gap_goal = 0.1
+        self.gap_goal = 0.
+        self.M = None
 
     def setup_random_data(self,
                           number_of_customers,
@@ -81,13 +94,13 @@ class VRP:
         self.y_range = y_range
         self.number_of_customers = number_of_customers
         self.demand_range = [demand_lower, demand_higher]
-        self.K = np.arange(1,number_of_vehicles+1)
+        self.K = np.arange(1, number_of_vehicles + 1)
 
         self.create_dataset()
 
         self.create_arcs()
 
-    def setup_preset_data(self, file_name, number_of_vehicles,subtour_type = 'DFJ'):
+    def setup_preset_data(self, file_name, number_of_vehicles, subtour_type='DFJ'):
 
         nb_customers, truck_capacity, n, v, demands, nodes = self.read_input_cvrp(file_name)
 
@@ -95,7 +108,7 @@ class VRP:
         self.Q = truck_capacity
         self.n = nb_customers + 1
         self.number_of_customers = nb_customers
-        self.K = np.arange(1,number_of_vehicles+1)
+        self.K = np.arange(1, number_of_vehicles + 1)
 
         self.nodes = nodes
         self.N = n
@@ -115,12 +128,13 @@ class VRP:
                  markersize=20, label='Depot', c='r', )
         plt.scatter(np.array(self.nodes.iloc[self.N])[:, 0],
                     np.array(self.nodes.iloc[self.N])[:, 1], c='b', label='Customer')
-        plt.annotate([self.V[0], self.V[-1]], (self.nodes.iloc[self.V[0]][0], self.nodes.iloc[self.V[0]][1]),
-                     xytext=(20, -20), textcoords='offset points', ha='right', va='bottom')
+        # plt.annotate([self.V[0], self.V[-1]], (self.nodes.iloc[self.V[0]][0], self.nodes.iloc[self.V[0]][1]),
+        #              xytext=(20, -20), textcoords='offset points', ha='right', va='bottom')
         for idx in self.V[1:-1]:
             plt.annotate(idx, (self.nodes.iloc[idx][0], self.nodes.iloc[idx][1]),
                          xytext=(5, 2), textcoords='offset points', ha='right', va='bottom')
         travel_paths = {}
+        times = {}
 
         for k in self.K:
             travel_paths[k] = []
@@ -130,20 +144,18 @@ class VRP:
                     travel_paths[k].append((i, j))
 
                     plt.plot([self.nodes.iloc[i][0], self.nodes.iloc[j][0]],
-                             [self.nodes.iloc[i][1], self.nodes.iloc[j][1]], c=cmap(k),zorder=0)
+                             [self.nodes.iloc[i][1], self.nodes.iloc[j][1]], c=cmap(k), zorder=0)
                     if (i, j) != (self.V[0], self.V[-1]) and (i, j) != (self.V[-1], self.V[0]):
                         plt.annotate(round(self.c[(i, j)], 2),
                                      ((self.nodes.iloc[i][0] + self.nodes.iloc[j][0]) / 2,
                                       (self.nodes.iloc[i][1] + self.nodes.iloc[j][1]) / 2),
                                      xytext=(20, 2), textcoords='offset points', ha='right', va='bottom')
-        print(self.V)
         print(travel_paths)
-        print(self.q)
         plt.legend(loc='lower right')
         plt.show()
+        return
 
-
-    ################################           DATASET        ###################################### 
+    ################################           DATASET        ######################################
     ################################################################################################
 
     def create_dataset(self):
@@ -154,6 +166,12 @@ class VRP:
 
     @staticmethod
     def calc_distance(p1, p2):
+        """
+        Distance between point p1 and point p2 in a 2-dimensional cartesian system.
+        :param p1: 1D iterable of size of 2
+        :param p2: 1D iterable of size of 2
+        :return: Distance (float)
+        """
         dist = (((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2) ** 0.5)
         return dist
 
@@ -173,6 +191,10 @@ class VRP:
         return nodes_table
 
     def create_customers(self):
+        """
+        Select customer nodes, set customer demands, and customer time windows.
+        :return:
+        """
         random_selection = list(self.nodes.sample(n=self.n + 1).index)
         # Select the depot node
         # Depot node is randomly selected
@@ -188,14 +210,13 @@ class VRP:
         # print("end_depot_idx", end_depot_idx)
 
         customer_nodes = self.nodes.to_numpy()[random_selection]
-        depot_nodes    = self.nodes.to_numpy()[ [start_depot_idx,end_depot_idx] ] 
+        depot_nodes = self.nodes.to_numpy()[[start_depot_idx, end_depot_idx]]
 
-        self.nodes     = pd.DataFrame( np.vstack( (depot_nodes[0], customer_nodes, depot_nodes[1]) ), columns=['x_coord', 'y_coord'])
+        self.nodes = pd.DataFrame(np.vstack((depot_nodes[0], customer_nodes, depot_nodes[1])),
+                                  columns=['x_coord', 'y_coord'])
 
-        self.N         = np.arange(1,self.n+1)
-        self.V         = np.hstack((0,self.N,self.n+1)) 
-
-        print(self.nodes)
+        self.N = np.arange(1, self.n + 1)
+        self.V = np.hstack((0, self.N, self.n + 1))
 
         # self.N = random_selection  # The rest will be the customer nodes
         # temp_list = self.N[:]
@@ -213,32 +234,45 @@ class VRP:
         #                                                      f"exceeds the total vehicle capacity: " \
         #                                                      f"{self.Q * len(self.K)} = {self.Q} * {len(self.K)}."
 
-        self.Q = sum(self.q.values()) // len(self.K)  + 1 # ensure that the demand can always be fulfilled
+        self.Q = sum(self.q.values()) // len(self.K) + 1  # ensure that the demand can always be fulfilled
+
+        if self.subtour_type is "TW":
+            # Assign time windows to each customer
+            self.e = {}
+            self.l = {}
+            self.p = {}
+            for i in self.N:
+                self.e[i] = np.random.randint(self.opening_time, self.closing_time - self.time_window)
+                self.l[i] = self.e[i] + self.time_window
+                self.p[i] = self.processing_time
 
         return
 
     def create_arcs(self):
-        # # Create arcs between customer nodes
-        # self.A = [c for c in itertools.product(self.N, self.N)]
+        """
+        Create arcs between customer nodes
+        :return:
+        """
+        self.A = [c for c in itertools.product(self.N, self.N)]
 
-        # # Add depot nodes
-        # # Make sure there are only leaving arcs from the depot start node...
-        # for j in self.V[1:]:
-        #     self.A.append((self.V[0], j))
-        # # ...and there are only leading nodes to the depot end node.
-        # for i in self.V[1:-1]:
-        #     self.A.append((i, self.V[-1]))
+        # Add depot nodes
+        # Make sure there are only leaving arcs from the depot start node...
+        for j in self.V[1:]:
+            self.A.append((self.V[0], j))
+        # ...and there are only leading nodes to the depot end node.
+        for i in self.V[1:-1]:
+            self.A.append((i, self.V[-1]))
 
-        # # Remove the elements where i=j
-        # for i, tup in enumerate(self.A):
-        #     if tup[0] == tup[1]:
-        #         self.A.pop(i)
+        # Remove the elements where i=j
+        for i, tup in enumerate(self.A):
+            if tup[0] == tup[1]:
+                self.A.pop(i)
 
-        # # Make sure there are no duplicate arcs
-        # duplicates = [(k, v) for k, v in Counter(self.A).items() if v > 1]
-        # assert len(duplicates) == 0, f"Duplicate arc found: {duplicates}"
+        # Make sure there are no duplicate arcs
+        duplicates = [(k, v) for k, v in Counter(self.A).items() if v > 1]
+        assert len(duplicates) == 0, f"Duplicate arc found: {duplicates}"
 
-        self.A = [(i, j) for i in self.V for j in self.V if i != j]    # arcs between nodes and vehicle k
+        # self.A = [(i, j) for i in self.V for j in self.V if i != j]  # arcs between nodes and vehicle k
 
         # The cost to travel an arc equals its length
         self.c = {}
@@ -250,11 +284,8 @@ class VRP:
             self.c[(i, j)] = self.calc_distance((x_i, y_i), (x_j, y_j))
         return
 
-
-
-    ################################           READ CVRP        #################################### 
+    ################################           READ CVRP        ####################################
     ################################################################################################
-
 
     ##################################################################################################
     # The reading functions are taken and modified from the original
@@ -366,61 +397,16 @@ class VRP:
 
         return nb_customers, truck_capacity, N, V, demands, nodes
 
-
-    ################################           OPTIMIZATION        ################################# 
+    ################################           OPTIMIZATION        #################################
     ################################################################################################
-    def setup(self):
-
-        self.model = Model()
-
-        # Variables
-        self.x = {}
-        for i, j in self.A:
-            for k in self.K:
-                self.x[i, j, k] = self.model.addVar(lb=0, ub=1, vtype=GRB.BINARY, name=f"x[{i},{j},{k}]")
-
-        self.u = {}
-        for j in self.N:
-            for k in self.K:
-                self.u[j, k] = self.model.addVar(vtype=GRB.CONTINUOUS, name=f"u[{j},{k}]")
-
-        # Make sure there are no duplicates of i,j,k combination in x
-        assert len(self.x) == len(set(self.x.keys()))
+    def CVRP_setup(self):
+        """
+        Additional constraints for subtour elimination from CVRP formulation
+        :return:
+        """
+        self.general_setup()
 
         self.model.update()
-
-        # Objective function
-        # explanation for self.V[:-1] = [0, 1, ..., n]
-        # explanation for self.V[1:] = [1, ..., n, n+1]
-        self.model.setObjective(quicksum(self.x[i, j, k] * self.c[i, j] for i in self.V[:-1] for j in self.V[1:]
-                                         for k in self.K if i != j),
-                                sense=GRB.MINIMIZE)
-        self.model.update()
-
-        # Constraints
-        # Each vehicle must leave the depot
-        for k in self.K:
-            self.model.addConstr(quicksum(self.x[self.V[0], j, k] for j in self.V[1:]) == 1, name=f"Start_{k}")
-        # Each vehicle must return to depot
-        for k in self.K:
-            self.model.addConstr(quicksum(self.x[j, self.V[-1], k] for j in self.V[:-1]) == 1, name=f"Finish_{k}")
-        # Each customer must be visited by one vehicle
-        for i in self.N:
-            self.model.addConstr(quicksum(self.x[j, i, k] for k in self.K for j in self.V[:-1] if j != i) == 1,
-                                 name=f"Visit_{i}")
-        # If a vehicle visits a customer, it must also leave that customer
-
-        for i in self.N:
-            for k in self.K:
-                self.model.addConstr(quicksum(self.x[j, i, k] for j in self.V[:-1] if j != i) ==  # What arrives to i
-                                     quicksum(self.x[i, j, k] for j in self.V[1:] if j != i),  # Must leave from i
-                                     name=f"Leave_{i}_{k}")
-
-        # Capacity constraint
-        for k in self.K:
-            self.model.addConstr(
-                quicksum(self.q[j] * self.x[i, j, k] for i in self.V[:-1] for j in self.V[1:-1] if i != j) <= self.Q,
-                name=f"Capacity_{k}")
 
         # # TODO: Sub-tour elimination via Miller-Tucker-Zemlin formulation
         # for k in self.K:
@@ -440,7 +426,8 @@ class VRP:
                 for i, j in self.A:
                     if i >= 1 and j >= 1:
                         if i != self.number_of_customers + 1 and j != self.number_of_customers + 1:
-                            self.model.addConstr(self.u[j, k] - self.u[i, k] >= self.q[j] - self.Q * (1 - self.x[i, j, k]))
+                            self.model.addConstr(
+                                self.u[j, k] - self.u[i, k] >= self.q[j] - self.Q * (1 - self.x[i, j, k]))
 
             # Capacity constraint
             for i in self.N:
@@ -453,19 +440,112 @@ class VRP:
         # $$\sum_{i\in S}\sum_{j \in S,j \neq i} x_{ij} \leq |S| - 1$$
         elif self.subtour_type == 'DFJ':
             for k in self.K:
-                self.model.addConstr(quicksum(quicksum(self.q[j] * self.x[i, j, k] for j in self.N if j != i) for i in self.V if i < self.number_of_customers + 1) <= self.Q)
+                self.model.addConstr(quicksum(
+                    quicksum(self.q[j] * self.x[i, j, k] for j in self.N if j != i) for i in self.V if
+                    i < self.number_of_customers + 1) <= self.Q)
 
         self.model.update()
 
-        # TODO: Set optimization limit
         self.model.setParam("MIPGap", self.gap_goal)
         self.model.update()
 
         self.model.write("model.lp")
 
+    def CVRPTW_setup(self):
+        """
+        Additional constraints from the time window formulation
+        :return:
+        """
+        self.subtour_type = "TW"
+
+        self.M = self.closing_time * 10  # Big-M formulation
+
+        self.general_setup()
+        self.model.update()
+
+        # Continuous decision variable for arrival to customer i
+        self.t = {}
+        for i in self.N:
+            self.t[i] = self.model.addVar(lb=self.opening_time, ub=self.closing_time,
+                                          vtype=GRB.CONTINUOUS, name=f"t[{i}]")
+
+        # Time window constraint
+        for i in self.N:
+            self.model.addConstr(self.t[i] >= self.e[i], name=f"Lower_time_{i}")
+            self.model.addConstr(self.t[i] <= self.l[i], name=f"Upper_time_{i}")
+            for j in self.N:
+                if i != j:
+                    self.model.addConstr(self.t[j] >=
+                                         self.t[i] + self.processing_time + self.c[i, j] -
+                                         (1 - quicksum(self.x[i, j, k] for k in self.K)) * self.M,
+                                         name=f"Time_precedence_{i}_{j}")
+
+        self.model.update()
+
+        self.model.setParam("MIPGap", self.gap_goal)
+        self.model.update()
+
+        self.model.write("model.lp")
+
+    def general_setup(self):
+        """
+        Basic model elements shared by both CVRP and CVRPTW
+        :return:
+        """
+
+        self.model = Model()
+
+        # Variables
+        self.x = {}
+        for i, j in self.A:
+            for k in self.K:
+                self.x[i, j, k] = self.model.addVar(lb=0, ub=1, vtype=GRB.BINARY, name=f"x[{i},{j},{k}]")
+
+        self.u = {}
+        for j in self.N:
+            for k in self.K:
+                self.u[j, k] = self.model.addVar(vtype=GRB.CONTINUOUS, name=f"u[{j},{k}]")
+
+        # Objective function
+        # explanation for self.V[:-1] = [0, 1, ..., n]
+        # explanation for self.V[1:] = [1, ..., n, n+1]
+        self.model.setObjective(quicksum(self.x[i, j, k] * self.c[i, j] for i in self.V[:-1] for j in self.V[1:]
+                                         for k in self.K if i != j),
+                                sense=GRB.MINIMIZE)
+
+        # Make sure there are no duplicates of i,j,k combination in x
+        assert len(self.x) == len(set(self.x.keys()))
+
+        self.model.update()
+
+        # Constraints
+        # Each vehicle must leave the depot
+        for k in self.K:
+            self.model.addConstr(quicksum(self.x[self.V[0], j, k] for j in self.V[1:]) == 1, name=f"Start_{k}")
+
+        # Each vehicle must return to depot
+        for k in self.K:
+            self.model.addConstr(quicksum(self.x[j, self.V[-1], k] for j in self.V[:-1]) == 1, name=f"Finish_{k}")
+
+        # Each customer must be visited by one vehicle
+        for i in self.N:
+            self.model.addConstr(quicksum(self.x[j, i, k] for k in self.K for j in self.V[:-1] if j != i) == 1,
+                                 name=f"Visit_{i}")
+
+        # If a vehicle visits a customer, it must also leave that customer
+        for i in self.N:
+            for k in self.K:
+                self.model.addConstr(quicksum(self.x[j, i, k] for j in self.V[:-1] if j != i) ==  # What arrives to i
+                                     quicksum(self.x[i, j, k] for j in self.V[1:] if j != i),  # Must leave from i
+                                     name=f"Leave_{i}_{k}")
+
+        # Capacity constraint
+        for k in self.K:
+            self.model.addConstr(
+                quicksum(self.q[j] * self.x[i, j, k] for i in self.V[:-1] for j in self.V[1:-1] if i != j) <= self.Q,
+                name=f"Capacity_{k}")
+
         return
-
-
 
     def optimize(self):
 
@@ -473,12 +553,31 @@ class VRP:
             self.model.optimize()
         elif self.subtour_type == 'DFJ':
             self.model.Params.lazyConstraints = 1
-            self.model._x = self.x # User made variables get passed along with underscore VarName
+            self.model._x = self.x  # User made variables get passed along with underscore VarName
             self.model._A = self.A
             self.model._K = self.K
             self.model._V = self.V
             self.model._subtour = self.subtour
             self.model.optimize(self.subtourelim)
+        elif self.subtour_type == "TW":
+            self.model.optimize()
+
+        if self.model.status == GRB.OPTIMAL:
+            print(f"Optimal objective: {self.model.objVal}")
+        elif self.model.status == GRB.INF_OR_UNBD:
+            print("Model is infeasible or unbounded")
+            sys.exit(0)
+        elif self.model.status == GRB.INFEASIBLE:
+            print("Model is infeasible")
+            self.model.computeIIS()
+            self.model.write("model.ilp")
+            sys.exit(0)
+        elif self.model.status == GRB.UNBOUNDED:
+            print("Model is unbounded")
+            sys.exit(0)
+        else:
+            print(f"Optimization ended with status {self.model.status}")
+            sys.exit(0)
 
         save_result = input("Save optimized result?:")
         if save_result.lower() in ['y', 'yes']:
@@ -501,7 +600,7 @@ class VRP:
             active_arcs = np.vstack(active_arcs)
 
             tours = mdl._subtour(mdl, active_arcs)
-            
+
             for k in tours.keys():
                 if len(tours[k]) > 1:
                     for tour in tours[k]:
